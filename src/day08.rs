767c99c0b3e23c5c2d1d2e1afb29deb221481bea
Hugo Tunius <h@tunius.se>
2018-12-09 14:14:37 +0000

    [Day 8] Simplify memory model

--- src/day08.rs
+++ src/day08.rs
@@ -1,24 +1,20 @@
-use std::cell::RefCell;
 use std::collections::VecDeque;
-use std::rc::Rc;
-
-type NodePointer = Rc<RefCell<Node>>;
 
 #[derive(Debug)]
 struct Node {
     metadata: Vec<usize>,
-    children: Vec<NodePointer>,
+    children: Vec<Node>,
 }
 
 impl Node {
     fn new(child_count: usize, metadata_count: usize) -> Node {
         Self {
             metadata: Vec::<usize>::with_capacity(metadata_count),
-            children: Vec::<NodePointer>::with_capacity(child_count),
+            children: Vec::<Node>::with_capacity(child_count),
         }
     }
 
-    fn add_children(&mut self, new_children: Vec<NodePointer>) {
+    fn add_children(&mut self, new_children: Vec<Node>) {
         self.children.extend(new_children);
     }
 
@@ -38,7 +34,7 @@ impl Node {
         if self.has_children() {
             self.metadata.iter().fold(0, |acc, &index| {
                 if index <= self.children.len() {
-                    acc + self.children[index - 1].borrow().value()
+                    acc + self.children[index - 1].value()
                 } else {
                     acc
                 }
@@ -48,10 +44,10 @@ impl Node {
         }
     }
 
-    fn recurse(numbers: &mut VecDeque<usize>) -> NodePointer {
+    fn recurse(numbers: &mut VecDeque<usize>) -> Node {
         let child_count = numbers.pop_front().unwrap();
         let metadata_count = numbers.pop_front().unwrap();
-        let node = Rc::new(RefCell::new(Self::new(child_count, metadata_count)));
+        let mut node = Self::new(child_count, metadata_count);
 
         let children = (0..child_count)
             .map(|_| Self::recurse(numbers))
@@ -61,24 +57,24 @@ impl Node {
             .map(|_| numbers.pop_front().unwrap())
             .collect::<Vec<_>>();
 
-        node.borrow_mut().add_children(children);
-        node.borrow_mut().add_metadata(metadata);
+        node.add_children(children);
+        node.add_metadata(metadata);
 
         node
     }
 
-    fn traverse<F>(root: &NodePointer, mut f: F)
+    fn traverse<F>(root: &Node, mut f: F)
     where
         F: FnMut(&Self),
     {
-        let mut to_visit: Vec<NodePointer> = vec![Rc::clone(root)];
+        let mut to_visit: Vec<&Node> = vec![root];
 
         while !to_visit.is_empty() {
             let next = to_visit.pop().unwrap();
-            f(&next.borrow_mut());
+            f(&next);
 
-            for child in next.borrow().children.iter() {
-                to_visit.push(Rc::clone(child));
+            for child in next.children.iter() {
+                to_visit.push(child);
             }
         }
     }
@@ -96,14 +92,12 @@ impl<'a> From<&'a str> for Node {
 
         let root = Node::recurse(&mut numbers);
 
-        Rc::try_unwrap(root)
-            .expect("Expect exactly one reference to the root node")
-            .into_inner()
+        root
     }
 }
 
 pub fn star_one(input: &str) -> usize {
-    let tree = Rc::new(RefCell::new(Node::from(input)));
+    let tree = Node::from(input);
     let mut sum = 0;
 
     Node::traverse(&tree, |node: &Node| sum += node.metadata_sum());
